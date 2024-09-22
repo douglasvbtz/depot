50f0600734d142a8886e1bf7b18a2a06ccb7e039
Douglas <douglasviebrantz@alunos.utfpr.edu.br>
2024-09-22 19:43:21 -0300

    feat: Create Carts controller, implement Add to Cart, Create Line Items

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -9,6 +9,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get products_url
     assert_response :success
+    assert_select 'h1', 'Products'
   end
 
   test "should get new" do
@@ -37,6 +38,13 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should update product" do
     patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @title } }
     assert_redirected_to product_url(@product)
+  end
+
+  test "can't delete product in cart" do
+    assert_difference("Product.count", 0) do
+      delete product_url(products(:two))
+    end
+    assert_redirected_to products_url
   end
 
   test "should destroy product" do
