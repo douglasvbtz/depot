fc5eaf93b91924389ec6ab30a1eef434d5ce5c6a
Douglas <douglasviebrantz@alunos.utfpr.edu.br>
2024-09-25 17:44:09 -0300

    feat: Integrate Hotwire for cart updates, move cart to sidebar, and add Turbo Streams support

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -3,23 +3,23 @@ require "test_helper"
 class CartTest < ActiveSupport::TestCase
   
   test "should add unique product to cart" do
-    cart = carts(:one)       # Obtém o carrinho de teste nomeado 'one'
-    product = products(:ruby) # Obtém o produto de teste nomeado 'ruby'
+    cart = carts(:one)
+    product = products(:ruby)
 
-    cart.add_product(product).save # Adiciona o produto ao carrinho
+    cart.add_product(product).save
 
-    assert_equal 1, cart.line_items.count # Verifica se há 1 item no carrinho
+    assert_equal 1, cart.line_items.count
   end
 
   test "should add duplicate product to cart" do
-    cart = carts(:one)       # Obtém o carrinho de teste nomeado 'one'
-    product = products(:ruby) # Obtém o produto de teste nomeado 'ruby'
+    cart = carts(:one)
+    product = products(:ruby)
 
-    cart.add_product(product).save # Adiciona o produto ao carrinho pela primeira vez
-    cart.add_product(product).save # Adiciona o mesmo produto novamente
+    cart.add_product(product).save
+    cart.add_product(product).save
 
-    assert_equal 1, cart.line_items.count # Verifica se ainda há apenas 1 item no carrinho
-    assert_equal 2, cart.line_items.first.quantity # Verifica se a quantidade do item é 2
+    assert_equal 1, cart.line_items.count
+    assert_equal 2, cart.line_items.first.quantity
   end
   
 end
