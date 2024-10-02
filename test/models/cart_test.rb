b2c38c649ac52a2ae4ac3a1c52b68339c601d848
Douglas <douglasviebrantz@alunos.utfpr.edu.br>
2024-10-02 20:10:10 -0300

    Adds button_to on image_tag for a product image, Updates test methods, Adds Turbo Stream to the decrement method

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,25 +1,25 @@
 require "test_helper"
 
 class CartTest < ActiveSupport::TestCase
-  
-  test "should add unique product to cart" do
-    cart = carts(:one)
-    product = products(:ruby)
 
-    cart.add_product(product).save
-
-    assert_equal 1, cart.line_items.count
+  def setup
+    @cart = Cart.create
+    @book_one = products(:ruby)
+    @book_two = products(:two)
   end
 
-  test "should add duplicate product to cart" do
-    cart = carts(:one)
-    product = products(:ruby)
+  test "add unique products" do
+    @cart.add_product(@book_one).save!
+    @cart.add_product(@book_two).save!
+    assert_equal 2, @cart.line_items.size
+    assert_equal @book_one.price + @book_two.price, @cart.total_price
+   end
+   test "add duplicate product" do
+    @cart.add_product(@book_one).save!
+    @cart.add_product(@book_one).save!
+    assert_equal 2*@book_one.price, @cart.total_price
+    assert_equal 1, @cart.line_items.size
+    assert_equal 2, @cart.line_items[0].quantity
+   end
 
-    cart.add_product(product).save
-    cart.add_product(product).save
-
-    assert_equal 1, cart.line_items.count
-    assert_equal 2, cart.line_items.first.quantity
-  end
-  
 end
