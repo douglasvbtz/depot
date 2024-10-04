0916945a3d4697cbc114868dbc5dbb839dc2aa14
Douglas <douglasviebrantz@alunos.utfpr.edu.br>
2024-10-04 19:29:20 -0300

    feat: Implement order mailer with received and shipped email templates, including email testing

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -26,11 +26,12 @@ class OrdersController <  ApplicationController
   def create
     @order = Order.new(order_params)
     @order.add_line_items_from_cart(@cart)
-  
+
     respond_to do |format|
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
+        OrderMailer.received(@order).deliver_later
         format.html { redirect_to store_index_url, notice: 'Thank you for your order.' }
         format.json { render :show, status: :created, location: @order }
       else
