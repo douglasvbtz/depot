3ec51b9678cc1c9f8d3baac7692bd3e96c3b6546
Douglas <douglasviebrantz@alunos.utfpr.edu.br>
2025-01-14 08:20:29 -0300

    feat: enhance order and store views, add i18n support

--- config/routes.rb
+++ config/routes.rb
@@ -5,18 +5,20 @@ Rails.application.routes.draw do
     post 'login' => :create
     delete 'logout' => :destroy
   end
-
   get 'logout' => 'sessions#destroy', as: :logout_via_get
+
   resources :users
-  resources :orders
-  resources :line_items do
-    put :decrement, on: :member
-  end
-  resources :carts
-  root 'store#index', as: 'store_index'
-  get 'store/index'
   resources :products do
     get :who_bought, on: :member
+  end
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items do
+      put :decrement, on: :member
+    end
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
